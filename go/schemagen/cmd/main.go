e1b4020d7fb3e40ffb0e66d6dc7313a23e4b08e4
Denis Saygin <oreshka.net@gmail.com>
2024-05-12 12:12:46 +0300

    Аргументы в генерации cmd

--- go/schemagen/cmd/main.go
+++ go/schemagen/cmd/main.go
@@ -3,24 +3,14 @@ package main
 import (
 	"github.com/kontora13-go/event-schema-registry/schemagen/gen"
 	"log"
+	"os"
 )
 
 func main() {
-	/*
-		schema, err := readCommonSchema("../schema/message/v1.json")
-		if err != nil {
-			log.Fatal(err)
-		}
-	*/
-
-	//log.Println(schema)
-
-	// TODO: from os.Args
-	g := gen.NewGen("./message", "../schema")
+	g := gen.NewGen(os.Args[0], os.Args[1])
 
 	err := g.Generate()
 	if err != nil {
 		log.Fatal(err)
 	}
-
 }
